b208c0bc9eba48f4965690e7e924619a98311e0b
jaywantvijaykumarpat <jaywantvijaykumarpat@cmail.carleton.ca>
2024-02-28 19:02:13 -0500

    Resolved the failing test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static String FizzBuzz(int x){
         String ans = " ";
-        if(x%3 == 0)
+        if(x%3 == 0 && x%5 == 0)
+            ans = "FizzBuzz";
+        else if(x%3 == 0)
             ans ="Fizz";
         else if(x%5 == 0)
             ans = "Buzz";
